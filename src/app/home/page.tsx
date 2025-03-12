1fd3dd1b9968b2e6d0b543c18ff0112cbe63b351
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2025-03-12 08:39:12 -0300

    att

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -29,53 +29,52 @@ export default function Home() {
   return (
     <div className="bg-gradient-to-t from-[#3d3d3d] to-[#3d3d3d] text-white">
   <main>
-    <div className="w-full h-[75vh] flex flex-col items-center md:relative bg-gradient-to-t from-brown-800 to-brown-400">
-      
-      <header className="absolute top-0 left-0 right-0 flex justify-between items-center bg-brown-500 p-6 md:p-12 z-20">
-        <h1 className="text-white text-2xl font-bold">Senha Pronta</h1>
-        <button className="md:hidden text-white" onClick={toggleMenu}>
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-        </button>
+  <div className="w-full h-[75vh] flex flex-col items-center md:relative bg-gradient-to-t from-brown-800 to-brown-400">
+  <header className="absolute top-0 left-0 right-0 flex justify-between items-center bg-brown-500 p-6 md:p-12 z-20">
+    <h1 className="text-white text-2xl font-bold">Senha Pronta</h1>
+    <button className="md:hidden text-white" onClick={toggleMenu}>
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+      </svg>
+    </button>
+    <nav className="space-x-10 hidden md:flex">
+      <a href="#" className="text-white hover:text-yellow-400">Vaquejadas</a>
+      <a href="#" className="text-white hover:text-yellow-400">Minhas Senhas</a>
+      <a href="#" className="text-white hover:text-yellow-400">Sobre</a>
+      <a href="#" className="text-white hover:text-yellow-400">Entrar</a>
+    </nav>
+  </header>
 
-        <nav className="space-x-10 hidden md:flex">
-          <a href="#" className="text-white hover:text-yellow-400">Vaquejadas</a>
-          <a href="#" className="text-white hover:text-yellow-400">Minhas Senhas</a>
-          <a href="#" className="text-white hover:text-yellow-400">Sobre</a>
-          <a href="#" className="text-white hover:text-yellow-400">Entrar</a>
-        </nav>
-      </header>
-
-      <div className="relative w-10/12 md:w-full h-auto md:h-full flex justify-center mt-10 md:mt-0">
-      <div className="relative w-full md:w-full h-104 md:h-auto mt-10 md:mt-0">
-        <img
-          src={NORDESTE.src}
-          alt="Imagem de fundo"
-          className="w-full h-full object-cover md:object-[center_50%] rounded-2xl md:rounded-none"
-        />
-        <div className="hidden md:block absolute inset-0 md:rounded-none bg-[radial-gradient(circle,_rgba(0,0,0,0)_1%,_rgba(0,0,0,0.8)_50%)] pointer-events-none"></div>
-      </div>
-
-
-        <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-black to-transparent rounded-b-2xl md:hidden"></div>
-
-        <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-4 w-10/12 justify-center md:bottom-30 md:left-10 md:translate-x-0 md:w-auto">
-          <button className="flex items-center gap-2 px-4 py-2 bg-white text-black font-semibold rounded-lg hover:bg-gray-200 transition shadow-lg md:px-8 md:py-4 md:text-lg text-sm cursor-pointer">
-            <img src={plus.src} alt="Play" className="w-3 h-3" />
-            Comprar
-          </button>
-          <button className="flex items-center gap-2 px-4 py-2 bg-gray-700 text-white font-semibold rounded-lg hover:bg-gray-800 transition shadow-lg md:px-8 md:py-4 md:text-lg text-sm cursor-pointer">
-            <img src={info.src} alt="Info" className="w-3 h-3" />
-            Info
-          </button>
-        </div>
-      </div>
-
-      <h1 className="hidden md:block absolute top-30 left-10 text-white text-4xl md:text-8xl font-bold max-w-lg drop-shadow-lg">
-        VAQUEJADA <span className='text-yellow-400'>RAIZ</span> AGILIDADE <span className='text-yellow-400'>MODERNA</span>
-      </h1>
+  <div className="relative w-70 md:w-full h-auto md:h-full flex justify-center mt-10 md:mt-0">
+    <div className="relative w-full md:w-full h-104 md:h-auto mt-10 md:mt-0">
+      <img
+        src={VAC9.src}
+        alt="Imagem de fundo"
+        className="w-full h-full object-cover md:object-[center_50%] rounded-2xl md:rounded-none"
+      />
+      <div className="hidden md:block absolute inset-0 bg-[radial-gradient(circle,_rgba(0,0,0,0)_1%,_rgba(0,0,0,0.8)_50%)] pointer-events-none"></div>
     </div>
+
+    <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-black to-transparent rounded-b-2xl md:hidden"></div>
+
+    <div className="hidden md:flex absolute inset-y-1/2 -translate-y-1/2 left-6 flex-col items-start w-5/12">
+      <h2 className="text-white text-3xl md:text-6xl font-bold mb-4 ml-4">40ª VAQUEJADA DO PARQUE PANTANAL</h2>
+      <p className="text-white text-lg md:text-xl max-w-lg mb-6 ml-4">Agilidade e praticidade para sua vaquejada. Compre já sua senha e participe!</p>
+    </div>
+    
+    <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex gap-4 w-10/12 justify-center md:bottom-10 md:left-10 md:translate-x-0 md:w-auto">
+      <button className="flex items-center gap-2 px-6 py-3 bg-yellow-400 text-black font-semibold rounded-lg hover:bg-yellow-500 transition shadow-lg md:px-12 md:py-5 md:text-xl text-xs cursor-pointer">
+        <img src={plus.src} alt="Comprar" className="w-4 h-4 md:w-6 md:h-6" />
+        Comprar
+      </button>
+      <button className="flex items-center gap-2 px-6 py-3 bg-gray-700 text-white font-semibold rounded-lg hover:bg-gray-800 transition shadow-lg md:px-12 md:py-5 md:text-xl text-xs cursor-pointer">
+        <img src={info.src} alt="Info" className="w-4 h-4 md:w-6 md:h-6" />
+        Info
+      </button>
+    </div>
+  </div>
+</div>
+
 
     <Vaquejadas/>
 
--- src/components/Vaquejadas/index.tsx
+++ src/components/Vaquejadas/index.tsx
@@ -11,48 +11,38 @@ import VAC10 from '../../../public/arari.jpg'
 import { useRef } from "react";
 import { ChevronLeft, ChevronRight, CheckCircle, CreditCard, Calendar, Info } from "lucide-react";
 
-const FOTO = { src: '../../../public/d7eb08c10c9dde392c3552eef0abf936.jpeg' };
-const FOTO2 = { src: '../../../public/cartaz.jpg'};
-const FOTO3 = { src: '../../../public/cartaz (1).jpg' };
-const FOTO4 = { src: '../../../public/cartaz (2).jpg'};
+const images = [VAC, VAC2, VAC3, VAC4];
 
 export default function VaquejadaSection() {
-    const scrollRef = useRef(null);
-
-
   return (
     <div>
       <section className="p-6 relative flex flex-col items-center">
       <h1 className="font-semibold text-4xl mb-6">Todas as Vaquejadas</h1>
-      
-      {/* Botões de navegação */}
-      <button 
-        
-        className="hidden md:flex absolute left-4 top-1/2 -translate-y-1/2 bg-black/50 p-3 rounded-full z-10"
-      >
-        <ChevronLeft size={40} className="text-white" />
-      </button>
-      <button 
-        
-        className="hidden md:flex absolute right-4 top-1/2 -translate-y-1/2 bg-black/50 p-3 rounded-full z-10"
-      >
-        <ChevronRight size={40} className="text-white" />
-      </button>
 
-      <div ref={scrollRef} className="hidden md:flex gap-8 overflow-x-scroll no-scrollbar w-full justify-center">
-        {[VAC, VAC2, VAC3, VAC4, VAC, VAC2].map((img, index) => (
-          <div key={index} className="bg-gray-800 h-[28rem] w-[22rem] rounded-lg overflow-hidden transform transition-all duration-300 hover:scale-105 hover:z-10 relative">
-            <img src={img.src} alt={`Vaquejada ${index + 1}`} className="object-cover w-full h-full cursor-pointer" />
-          </div>
-        ))}
-      </div>
+      <div className="relative flex items-center justify-center w-full">
+        <button className="hidden md:flex absolute left-4 bg-black/50 p-3 rounded-full z-10">
+          <ChevronLeft size={40} className="text-white" />
+        </button>
 
-      <div className="md:hidden flex overflow-x-auto space-x-4 pb-4 w-full px-6">
-        {[VAC, VAC2, VAC3, VAC4].map((img, index) => (
-          <div key={index} className="bg-gray-800 h-96 min-w-[80%] rounded-lg overflow-hidden">
-            <img src={img.src} alt={`Vaquejada ${index + 1}`} className="object-cover w-full h-full" />
-          </div>
-        ))}
+        <div className="hidden md:flex md:ml-30 md:mr-30 gap-4 justify-center overflow-hidden">
+          {images.map((img, index) => (
+            <div key={index} className="bg-gray-800 h-[40rem] w-[30rem] rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-105">
+              <img src={img.src} alt={`Vaquejada ${index + 1}`} className="object-cover w-full h-full" />
+            </div>
+          ))}
+        </div>
+
+        <div className="md:hidden flex overflow-x-auto space-x-4 pb-4 w-full px-6">
+          {images.slice(0, 4).map((img, index) => (
+            <div key={index} className="bg-gray-800 h-96 min-w-[80%] rounded-lg overflow-hidden">
+              <img src={img.src} alt={`Vaquejada ${index + 1}`} className="object-cover w-full h-full" />
+            </div>
+          ))}
+        </div>
+
+        <button className="hidden md:flex absolute right-4 bg-black/50 p-3 rounded-full z-10">
+          <ChevronRight size={40} className="text-white" />
+        </button>
       </div>
     </section>
 
@@ -85,8 +75,8 @@ export default function VaquejadaSection() {
         </div>
       </section>
 
-      <section className="mt-12 bg-gray-100 w-full py-12 flex flex-col items-center text-center px-6">
-        <h2 className="text-3xl font-bold mb-4">Gerencie suas Vaquejadas com Facilidade</h2>
+      <section className="mt-0 bg-gray-100 w-full h-screen md:h-120 py-12 flex flex-col items-center text-center px-6">
+        <h2 className="text-3xl font-bold mb-4 text-gray-900">Gerencie suas Vaquejadas com Facilidade</h2>
         <p className="text-lg text-gray-600 max-w-2xl">
           Nosso sistema de controle de vaquejadas permite a venda de senhas de forma organizada e eficiente.
           Tenha total controle sobre suas vendas e melhore sua gestão com nossa tecnologia intuitiva.
@@ -94,7 +84,7 @@ export default function VaquejadaSection() {
         
         <div className="flex gap-6 mt-6">
           <button className="bg-blue-600 text-white px-6 py-3 rounded-lg text-lg font-semibold hover:bg-blue-700 transition">
-            Contratar Serviço
+            Quero comprar
           </button>
           <button className="border-2 border-blue-600 text-blue-600 px-6 py-3 rounded-lg text-lg font-semibold hover:bg-blue-600 hover:text-white transition">
             Entrar em Contato
@@ -103,10 +93,12 @@ export default function VaquejadaSection() {
       </section>
 
       <footer className="bg-gray-900 text-white w-full py-8 flex flex-col items-center text-center">
-        <p className="text-lg">© 2025 Sistema de Vaquejada. Todos os direitos reservados.</p>
+        <p className="text-lg">Senha pronta</p>
         <div className="flex gap-6 mt-4">
-          <a href="#" className="text-blue-400 hover:underline">Política de Privacidade</a>
-          <a href="#" className="text-blue-400 hover:underline">Termos de Uso</a>
+          <a href="#" className="text-blue-400 hover:underline">Vaquejadas</a>
+          <a href="#" className="text-blue-400 hover:underline">Senhas</a>
+          <a href="#" className="text-blue-400 hover:underline">Sobre</a>
+          <a href="#" className="text-blue-400 hover:underline">Entrar</a>
         </div>
       </footer>
     </div>
